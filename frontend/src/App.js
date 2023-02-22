138e113d202aade3528886a0a8ec0e344cbf9167
chloemaybrandon <chloemaybrandon@gmail.com>
2023-02-21 16:12:30 -0800

    Implement routing to navigate from login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 //import logo from './logo.svg';
+import {Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Pages/Login'
+import SearchListings from './Pages/SearchListings'
+import CreateAccount from './Pages/CreateAccount'
 
 function App() {
   return (
@@ -19,7 +22,24 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <Login/>
+
+      <ul>
+        {/* note: Link underlying type is anchor */}
+        <li><Link to='/search-listings'>Login (to SearchListings)</Link></li>
+        <li><Link to='/create-account'>Create account</Link></li>
+      </ul>
+
+      {/* successfully logging in will take you to search page */}
+      <button><Link to='/search-listings'>Login</Link></button>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/search-listings' element={<SearchListings />}/>
+        <Route path='/create-account' element={<CreateAccount />}/>
+        <Route />
+      </Routes>
+
+      
+      {/* <Login/> */}
     </div>
   );
 }
