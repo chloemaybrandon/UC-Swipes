4690bfdcf4cfb233d1e01f63b60236feaffcfdce
chloemaybrandon <chloemaybrandon@gmail.com>
2023-02-22 19:17:00 -0800

    Add a navbar with routing between pages

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,9 +1,15 @@
 //login will need to route to main page OR create account page
+import {Link} from 'react-router-dom';
 
 export default function Login(){
     return(
+        
         <div>
             <p>This is the login page. This should be the first page displayed when loading the site.</p>
+                  {/* successfully logging in will take you to search page */}
+            <button><Link to='/search-listings'>Login</Link></button>
+            <br/>
+            <button><Link to='/create-account'>Create an Account</Link></button>
         </div>
     )
 }
