f36a7203e85443618207f77d67df9c3b9d65023e
kenancackovic <kenanc@ucla.edu>
2023-03-07 11:01:36 -0800

    Updated CreatePost.js
    Added the html and form to fill out for creating a listing.

--- frontend/src/Pages/CreatePost.js
+++ frontend/src/Pages/CreatePost.js
@@ -1,6 +1,6 @@
 //creating a post will need to route to main page
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 // export default function CreateAccount() {
 //     const [email, setEmail] = useState("");
@@ -68,9 +68,46 @@
 
 
 export default function CreatePost(){
+    // const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [location, setLocation] = useState("");
+    // const [password, setPass] = useState("");
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Created an Listing");
+    };
+    
+    
+    
     return(
         <div>
+            <h2>Create a Listing</h2>
             <p>This is the create post page. This can be navigated to from PersonalListings.</p>
+            <form>
+                <label htmlFor="price">Price Per Swipe </label>
+                    <input
+                        value={price}
+                        name="price"
+                        onChange={(e) => setPrice(e.target.value)}
+                        id="price"
+                        placeholder="Price"
+                    />
+                    <br />
+                <label htmlFor="location">Choose a location </label>
+                    <select name="location" value={location} onChange={(e) => setLocation(e.target.value)}>
+                        <option value="rieber">Rieber</option>
+                        <option value="hedrick">Hedrick</option>
+                        <option value="sproul">Sproul</option>
+                        <option value="de_neve">De Neve</option>
+                    </select>
+                    <br />
+                    <br />
+                <button onClick={handleSubmit}>
+                    <Link to="/search-listings">Publish Post</Link>
+                </button>
+            </form>
         </div>
     )
 }
