c357ff74f0ae54e968d552d55f46f8d2e7fd7e83
Andrew Pan <andrewpan07@gmail.com>
2023-03-09 19:36:27 -0800

    fix an html error within <table>

--- frontend/src/Components/listing.js
+++ frontend/src/Components/listing.js
@@ -1,29 +1,28 @@
-import '../CSS/listing.css';
-import {Link} from 'react-router-dom';
+import "../CSS/listing.css";
+import { Link } from "react-router-dom";
 import React, { useState } from "react";
 
 export const Listing = () => {
-    return(
+    return (
         <React.Fragment>
-        <div className='listing_div'>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Location</th>
-                <th>Amount</th>
-                <th>Time</th>
-                <th>Date</th>  
-            </tr>
-        </table>
-            <p>This is a listing component</p>
-            <br />
-            <p>End of listing</p>
-        </div>
+            <div className="listing_div">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Location</th>
+                            <th>Amount</th>
+                            <th>Time</th>
+                            <th>Date</th>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>This is a listing component</p>
+                <br />
+                <p>End of listing</p>
+            </div>
         </React.Fragment>
-        
-
-
-    )
-}
+    );
+};
