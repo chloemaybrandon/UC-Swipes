67066b8c31231ec498f4217c41efcdac575433b6
kenancackovic <kenanc@ucla.edu>
2023-03-11 13:36:25 -0800

    Added a search bar feature to search all listings
    Added a new function in SearchListings.js that will only display the listings whose seller's name matches what is entered in the seller search bar

--- frontend/src/Pages/SearchListings.js
+++ frontend/src/Pages/SearchListings.js
@@ -1,4 +1,4 @@
-import { Listing } from "../Components/listing"
+// import { Listing } from "../Components/listing"
 import '../CSS/SearchListings.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
@@ -8,7 +8,11 @@ export default function SearchListings(){
     let [listings, setListings] = useState([]);
 
     const URL = "http://localhost:8080"
-  
+    
+    // This  
+    const [search_by_name, setSearch_by_name] = useState("");
+    const [searchParam] = useState(["poster_name"]);
+
     //function to get the listings
     const getListings = () => {
       // "/listings" retrieves from mongoDB endpoint in a response object
@@ -32,12 +36,43 @@ export default function SearchListings(){
       getListings();
     }, []);
 
+    function search(listings) {
+      return listings.filter((listings) => {
+          return searchParam.some((newItem) => {
+              return (
+                listings[newItem]
+                      .toString()
+                      .toLowerCase()
+                      .indexOf(search_by_name.toLowerCase()) > -1
+              );
+          });
+      });
+  }
+
     return(
         <div>
             <p>This is the SearchListings page. This can be navigated to from the navbar and is the first thing that should appear after logging in.</p>
             
             <div className="axios_lisitng_container">
-            {listings.map(listing =>
+            <div className="search-wrapper">
+              <label htmlFor="search-form">
+                <span className="sr-only">Search by Seller's Name </span>
+                <input
+                    type="search"
+                    name="search-form"
+                    id="search-form"
+                    className="search-input"
+                    placeholder="Search for Sellers"
+                    value={search_by_name}
+                    /*
+                    // set the value of our useState q
+                    //  anytime the user types in the search box
+                    */
+                    onChange={(search_by_name) => setSearch_by_name(search_by_name.target.value)}
+                />
+              </label>
+            </div>
+            {search(listings).map(listing =>
                     <div className="axios_lisitng">
                         <h3>Seller: {listing.poster_name}</h3>
                         <p>Where to meet: {listing.location}</p>
